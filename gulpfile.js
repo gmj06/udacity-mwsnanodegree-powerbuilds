4a0d446ff3c6408c4b647eafcf0db76d64defbd5
Gowri Jagadeesh <gowrimj2006@gmail.com>
2018-06-30 20:48:30 -0700

    Installed Browser-sync globally and in Sublime editor for live editing

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-
+var browserSync = require('browser-sync').create();
 gulp.task("default", function(){
-    gulp.watch("app/sass/**/*.scss", ["styles"])
+    gulp.watch("app/sass/**/*.scss", ["styles"]);
+
+    browserSync.init({
+        server: './'
+    });
 });
 
 gulp.task("styles", function(){
@@ -13,4 +17,5 @@ gulp.task("styles", function(){
         browsers: ['last 2 versions']
     }))
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream())
 });
