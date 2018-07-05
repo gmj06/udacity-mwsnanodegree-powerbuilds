05b1bf1440346bf9b206c9e7a73a5fc9f0943c8b
Gowri Jagadeesh <gowrimj2006@gmail.com>
2018-07-04 19:51:06 -0700

    Installed gulp-jasmine-phantom, added tests folder for unit testing and dist folder to maintain production level code
    Modified gulpfile.js
    1. to copy and watch index.html to dist folder
    2. to copy images to dist/img
    3. added tests task to run the tests from the spec

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,63 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
-gulp.task("default", function(){
-    gulp.watch("app/sass/**/*.scss", ["styles"]);
+var eslint = require('gulp-eslint');
+var jasmine = require('gulp-jasmine-phantom');
+
+gulp.task('default', ['copy-html', 'copy-images', 'styles', 'lint'], function(){
+    gulp.watch('app/sass/**/*.scss', ['styles']);
+    gulp.watch('app/js/**/*.js', ['lint']);
+    gulp.watch('app/index.html', ['copy-html']);
+    gulp.watch('./dist/index.html').on('change', browserSync.reload);
 
     browserSync.init({
-        server: './'
-    });
+        server: './dist'//,
+        //index: 'index.html'
+    });    
 });
 
-gulp.task("styles", function(){
-    return gulp.src("app/sass/**/*.scss")
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions']
-    }))
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.stream())
+gulp.task('copy-html', function(){
+    gulp.src('app/index.html')
+    .pipe(gulp.dest('./dist'));
+    //.pipe(browserSync.stream());
+});
+
+gulp.task('copy-images', function(){
+    gulp.src('app/img/*')
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('styles', function(){
+    gulp.src('app/sass/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('lint', () => {
+    // ESLint ignores files with "node_modules" paths.
+    // So, it's best to have gulp ignore the directory as well.
+    // Also, Be sure to return the stream from the task;
+    // Otherwise, the task may end before the stream has finished.
+    return gulp.src(['app/js/**/*.js','!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('tests', function() {
+    return gulp.src('app/tests/spec/extraSpec.js')
+    .pipe(jasmine({
+        integration: true,
+        vendor: 'app/js/**/*.js'
+    }));
 });
